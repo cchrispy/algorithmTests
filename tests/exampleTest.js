060c97339082fa6905eb5d9d7c0664ab3dfc790c
Chris Chiang <cchiang1992@gmail.com>
2016-12-06 17:17:13 -0800

    Example tests

--- tests/exampleTest.js
+++ tests/exampleTest.js
@@ -1,8 +1,46 @@
-var expect = require('chai').expect;
-var example = require('../src/example.js');
+import { expect } from 'chai';
+import { addOne, arrayShift } from '../src/example.js';
 
-describe('Array', () => {
-  it('should work', () => {
-    expect(2).to.equal(1);
+describe('Example', () => {
+
+  describe('expect', () => {
+
+    it('Should be a function', () => {
+      expect(expect).to.be.a('function');
+    })
+
   })
+
+  describe('ES6', () => {
+
+    it('Mocha should be compiling ES6', () => {
+      expect((() => 10)()).to.equal(10);
+    })
+    it('Should import functions from src', () => {
+      expect(addOne(2)).to.equal(3);
+      expect(arrayShift([0, 1, 2])).to.eql([1, 2]);
+    })
+
+  })
+
+  describe('Nested describe statements', () => {
+
+    it('Should work', () => {
+      expect(2).to.equal(2);
+    })
+
+    describe('Here', () => {
+
+      it('Works here too', () => {
+        expect(undefined).to.be.undefined;
+      })
+
+    })
+
+  })
+
+  it('Should work', () => {
+    expect(0).to.equal(0);
+  })
+
 })
