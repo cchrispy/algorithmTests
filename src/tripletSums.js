bd9192a8648df06ba15da6b5ec607e0c80787e9c
Chris Chiang <cchiang1992@gmail.com>
2016-12-12 15:58:50 -0800

    tests for tripletSums

--- src/tripletSums.js
+++ src/tripletSums.js
@@ -2,7 +2,9 @@
 // find all the triplets that will add up to a sum greater than or equal to N.
 
 var tripletSums = (arr, n) => {
-
+  if (!Array.isArray(arr) || arr.length < 3 || typeof n !== 'number') {
+    return null;
+  }
 }
 
 export default tripletSums;
--- tests/tripletSumsTest.js
+++ tests/tripletSumsTest.js
@@ -5,6 +5,18 @@ describe('Triplet Sums', () => {
 
   it('Should be a function', () => {
     expect(tripletSums).to.be.a('function');
+  });
+
+  it('Should accept an array and a number as arguments', () => {
+    expect(tripletSums()).to.be.null;
+    expect(tripletSums('hey', 40)).to.be.null;
+    expect(tripletSums([], 40)).to.be.null;
+    expect(tripletSums([1, 2, 3])).to.be.null;
+    expect(tripletSums([1, 2], 0)).to.be.null;
+  });
+
+  it('Should return an array', () => {
+    expect(tripletSums([10, 20, 30, 40], 30)).to.be.an('array');
   })
 
 });
