d94634c6bbe2605f616554cc07e661ea5ac2c48a
Chris Chiang <cchiang1992@gmail.com>
2016-12-12 15:54:02 -0800

    TripletSums added

---
+++ src/tripletSums.js
@@ -0,0 +1,8 @@
+// Given an array of integers and a number N, 
+// find all the triplets that will add up to a sum greater than or equal to N.
+
+var tripletSums = (arr, n) => {
+
+}
+
+export default tripletSums;
---
+++ tests/tripletSumsTest.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import tripletSums from '../src/tripletSums';
+
+describe('Triplet Sums', () => {
+
+  it('Should be a function', () => {
+    expect(tripletSums).to.be.a('function');
+  })
+
+});
