0dbde1d00800300f6f471c2129d0292775e88324
Chris Chiang <cchiang1992@gmail.com>
2016-12-15 18:57:47 -0800

    trie comments on methods

--- src/trie.js
+++ src/trie.js
@@ -7,18 +7,27 @@ class Trie {
   }
 
   addWord(string) {
+    // Iterate through the letters and check if they exist in the trie
+    // Add new letters to the existing prefix as new nodes
 
   }
 
   removeWord(string) {
+    // Go through the trie until you hit a non-branching node
+    // Delete that to remove the word
+    // Ensure that you do not delete prefixes that extend to other words
 
   }
 
   isMember(string) {
+    // Verify if the word exists in the trie
+    // Iterate through the letters and trace it in the trie
     return false;
   }
 
   predict(prefix) {
+    // Iterate to the last letter/node
+    // Record all complete branches of the node (down to the leaves)
     var predictions = [];
 
     return predictions;
@@ -28,6 +37,7 @@ class Trie {
 
 class Node {
   constructor(val) {
+    // Each node holds one letter
     this.val = val;
     this.children = [];
   }
