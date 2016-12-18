5396db71a710cf433930cccc99e5e49c136674bd
Chris Chiang <cchiang1992@gmail.com>
2016-12-17 16:19:26 -0800

    Init directedGraph

---
+++ src/directedGraph.js
@@ -0,0 +1,32 @@
+// Implement a directed graph
+// It is a collection of nodes that have edges between some of them
+// Directed edges point in one direction (undirected edges point both ways)
+
+class directedGraph {
+  constructor() {
+    this.nodes = {};
+  }
+
+  addNode(val) {
+
+  }
+
+  addEdge(node1, node2) {
+
+  }
+
+  removeNode(node) {
+
+  }
+
+  removeEdge(node1, node2) {
+
+  }
+
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+  }
+}
---
+++ tests/directedGraphTest.js
@@ -0,0 +1,2 @@
+import { expect } from 'chai';
+import directedGraph from '../src/directedGraph';
