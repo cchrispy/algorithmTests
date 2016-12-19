a8dfe697e01ee2ed51a1de25b9f8e63cfbcc26b9
Chris Chiang <cchiang1992@gmail.com>
2016-12-18 18:45:03 -0800

    binary search tree complete

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -2,7 +2,7 @@
 
 class binarySearchTree {
   constructor(x) {
-    this.val = x || null;
+    this.val = x !== undefined ? x : null;
     this.left = null;
     this.right = null;
   }
@@ -35,10 +35,91 @@ class binarySearchTree {
 
   delete(x) {
 
+
+
   }
 
   lookup(x) {
+    // Depth first search
 
+    if (this.val === x) { return true; }
+
+    if (x <= this.val) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.lookup(x);
+      }
+    } else {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.lookup(x);
+      }
+    }
+
+  }
+
+  getMin() {
+    if (this.left === null) {
+      return this.val;
+    }
+    return this.left.getMin();
+  }
+
+  getMax() {
+    if (this.right === null) {
+      return this.val;
+    }
+    return this.right.getMax();
+  }
+
+  deleteMin() {
+    if (this.left === null) { // if the root is the min, adjust the new root
+      var min = this.val;
+
+      if (this.right) {
+        this.val = this.right.val;
+        this.left = this.right.left;
+        this.right = this.right.right;
+      } else {
+        this.val = null;
+      }
+
+      return min;
+    }
+
+    if (this.left.left === null) {
+      var min = this.left.val;
+      this.left = this.left.right;
+      return min;
+    } else {
+      return this.left.deleteMin();
+    }
+  }
+
+  deleteMax() {
+    if (this.right === null) { // if the root is the max
+      var max = this.val;
+
+      if (this.left) {
+        this.val = this.left.val;
+        this.right = this.left.right;
+        this.left = this.left.left;
+      } else {
+        this.val = null;
+      }
+
+      return max;
+    }
+
+    if (this.right.right === null) {
+      var max = this.right.val;
+      this.right = this.right.left;
+      return max;
+    } else {
+      return this.right.deleteMax();
+    }
   }
 
 }
--- tests/binarySearchTreeTest.js
+++ tests/binarySearchTreeTest.js
@@ -8,26 +8,111 @@ describe('Binary Search Tree', () => {
     tree = new binarySearchTree();
   });
 
+  const insert = (bst, arr) => arr.forEach(x => bst.insert(x));
+
   it('Should be a class', () => {
     expect(tree).to.be.an.instanceof(binarySearchTree);
   });
 
-  it('Should have "insert", "delete", "lookup" methods', () => {
+  it('Should have "insert", "delete", "lookup", "getMin", "getMax", "deleteMin", "deleteMax" methods', () => {
 
     expect(tree.insert).to.be.a('function');
     expect(tree.delete).to.be.a('function');
     expect(tree.lookup).to.be.a('function');
+    expect(tree.getMin).to.be.a('function');
+    expect(tree.getMax).to.be.a('function');
+    expect(tree.deleteMin).to.be.a('function');
+    expect(tree.deleteMax).to.be.a('function');
 
   });
 
-  xit('Should insert numbers in appropriate places', () => {
+  it('Every node in the tree should be its own binarySearchTree', () => {
+    var values = [5, 2, 4, 9, 10, 14, 1, 3, 6];
+    insert(tree, values);
+
+    expect(tree.left).to.be.an.instanceof(binarySearchTree);
+    expect(tree.left.right).to.be.an.instanceof(binarySearchTree);
+    expect(tree.right).to.be.an.instanceof(binarySearchTree);
+    expect(tree.right.right).to.be.an.instanceof(binarySearchTree);
+    expect(tree.right.left).to.be.an.instanceof(binarySearchTree);
+  });
+
+  it('Should be able to get min and max values', () => {
+    var values = [5, 2, 4, 9, 10, 14, 1, 3, 6];
+    insert(tree, values);
+
+    expect(tree.getMin()).to.equal(1);
+    expect(tree.getMax()).to.equal(14);
+
+    insert(tree, [0]);
+
+    expect(tree.getMin()).to.equal(0);
 
   });
 
-  xit('Every node in the tree should be its own binarySearchTree', () => {
+  describe('deleteMin and deleteMax methods', () => {
 
-  });
+    var tree_1;
+    beforeEach(() => {
+      tree_1 = new binarySearchTree();
+    })
 
-  
+    it('Normal case', () => {
+      var values = [5, 2, 4, 9, 10, 14, 1, 3, 6];
+      insert(tree_1, values);
+
+      tree_1.deleteMin();
+      tree_1.deleteMax();
+
+      expect(tree_1.getMin()).to.equal(2);
+      expect(tree_1.getMax()).to.equal(10);
+
+      tree_1.deleteMax();
+
+      expect(tree_1.getMax()).to.equal(9);
+
+    });
+
+    it('If the root is the min', () => {
+      var values = [1, 4, 5, 6, 2, 3];
+      insert(tree_1, values);
+
+      expect(tree_1.getMin()).to.equal(1);
+      expect(tree_1.getMax()).to.equal(6);
+
+      tree_1.deleteMin();
+
+      expect(tree_1.getMin()).to.equal(2);
+      expect(tree_1.getMax()).to.equal(6);
+
+      tree_1.deleteMax();
+
+      expect(tree_1.getMax()).to.equal(5);
+
+    });
+
+    it('If the root is the max', () => {
+      var values = [10, 9, 2, 4, 7, 8, 1];
+      insert(tree_1, values);
+
+      expect(tree_1.getMin()).to.equal(1);
+      expect(tree_1.getMax()).to.equal(10);
+
+      tree_1.deleteMin();
+      tree_1.deleteMax();
+
+      expect(tree_1.getMin()).to.equal(2);
+      expect(tree_1.getMax()).to.equal(9);
+
+      tree_1.deleteMax();
+
+      expect(tree_1.getMax()).to.equal(8);
+      expect(tree_1.getMin()).to.equal(2);
+
+    })
+    
+  })
+
+
 
 })
