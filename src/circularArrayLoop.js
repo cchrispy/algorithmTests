f20f7025ece8d136ff355edb2576c6ea6621a22b
Chris Chiang <cchiang1992@gmail.com>
2017-01-02 15:34:14 -0800

    90% complete circularArrayLoop function; does not handle changing directional loops

--- src/circularArrayLoop.js
+++ src/circularArrayLoop.js
@@ -5,13 +5,40 @@
 // if it starts and ends at a certain index, and has MORE THAN 1 element. The loop must go
 // either 'forward' or 'backwards'.
 // Example: [2, -1, 1, 2, 2] has a loop: index 0 -> 2 -> 3 -> 0
-// Example: [-1 2] has no loop
+// Example: [-1, 2] has no loop
 // O(n) time complexity; O(1) space complexity
 
 var circularArrayLoop = arr => { // returns a boolean value
 
-  
+  /*
+  ** This solution does not work for loops that have parts of it
+  ** going in different directions.... for example:
+  **           [1, -1] should be false but it's true
+  ** [-4, 2, 4, -1, 1] should be false but it's true
+  */
 
+  var index = 0;
+  var prev;
+
+  for (var i = 0; i < arr.length; i++) {
+
+    prev = index;
+
+    var steps = arr[index];
+    if (steps === arr.length) { return false; } // handles a loop with 1 element
+    arr[index] = 0;
+    index += steps;
+
+    if (index >= arr.length) { index -= arr.length; }
+    if (index < 0) { index += arr.length; }
+
+    if (prev === index) { return false; } // handles a loop with 1 element
+
+    if (!arr[index]) { return true; }
+
+  }
+
+  return false;
 }
 
 export default circularArrayLoop;
--- tests/circularArrayLoopTest.js
+++ tests/circularArrayLoopTest.js
@@ -4,16 +4,48 @@ import circularArrayLoop from '../src/circularArrayLoop';
 describe('Circular Array Loop', () => {
 
   it('[2, -1, 1, 2, 2] should have a loop', () => {
-    
+    var arr = [2, -1, 1, 2, 2];
+    expect(circularArrayLoop(arr)).to.be.true;
   });
 
   it('[-1, 2] should not have a loop', () => {
-    
+    var arr = [-1, 2];
+    expect(circularArrayLoop(arr)).to.be.false;
   });
 
   it('[1, 2, 3, 4, 5] should have a loop', () => {
-    
+    var arr = [1, 2, 3, 4, 5];
+    expect(circularArrayLoop(arr)).to.be.true;
   });
 
+  it('[1, 1] should not have a loop', () => {
+    var arr = [1, 1];
+    expect(circularArrayLoop(arr)).to.be.true;
+  })
+
+  it('[2, 2] should not have a loop', () => {
+    var arr = [2, 2];
+    expect(circularArrayLoop(arr)).to.be.false;
+  })
+
+  it('[2, 2, 2] should have a loop', () => {
+    var arr = [2, 2, 2];
+    expect(circularArrayLoop(arr)).to.be.true;
+  })
+
+  it('[2, 1, -4, -1, 6] should have a loop', () => {
+    var arr = [2, 1, -4, -1, 6];
+    expect(circularArrayLoop(arr)).to.be.true;
+  })
+
+  xit('[1, -1] should not have a loop (because of changing directions)', () => {
+    var arr = [1, -1];
+    expect(circularArrayLoop(arr)).to.be.false;
+  })
+
+  xit('[-4, 2, 4, -1, 1] should not have a loop (because of changing directions)', () => {
+    var arr = [-4, 2, 4, -1, 1];
+    expect(circularArrayLoop(arr)).to.be.false;
+  })
 
 })
