65142ffc39c2aa9c1d595cd57fe681ca4a1d3213
Chris Chiang <cchiang1992@gmail.com>
2017-01-02 15:00:09 -0800

    Add circular array loop function

---
+++ src/circularArrayLoop.js
@@ -0,0 +1,17 @@
+// You are given an array of positive or negative integers (no 0). If a number n is positive,
+// then move forward n steps. If n is negative, then move backwards n steps. Assume the first
+// element wraps ahead of the last element, and the last element wraps behind the first element.
+// Starting at index 0, determine if there is a loop in the array. Something is considered a loop
+// if it starts and ends at a certain index, and has MORE THAN 1 element. The loop must go
+// either 'forward' or 'backwards'.
+// Example: [2, -1, 1, 2, 2] has a loop: index 0 -> 2 -> 3 -> 0
+// Example: [-1 2] has no loop
+// O(n) time complexity; O(1) space complexity
+
+var circularArrayLoop = arr => { // returns a boolean value
+
+  
+
+}
+
+export default circularArrayLoop;
---
+++ tests/circularArrayLoopTest.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import circularArrayLoop from '../src/circularArrayLoop';
+
+describe('Circular Array Loop', () => {
+
+  it('[2, -1, 1, 2, 2] should have a loop', () => {
+    
+  });
+
+  it('[-1, 2] should not have a loop', () => {
+    
+  });
+
+  it('[1, 2, 3, 4, 5] should have a loop', () => {
+    
+  });
+
+
+})
