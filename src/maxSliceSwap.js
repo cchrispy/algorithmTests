64a14a73c643bc15a949ebd5ef493a8174bc2734
Chris Chiang <cchiang1992@gmail.com>
2017-02-07 20:36:53 -0800

    Add maxSliceSwap algorithm

---
+++ src/maxSliceSwap.js
@@ -0,0 +1,15 @@
+// Given an array, find the maximum sum from the set of subarrays after performing a single swap.
+
+/*
+** Example
+** [-3, 5, -2, 3, 4, -2, 3] should be 13 because you swap 5 with -2
+** [-3, 5, -2, 3, -4, 4, -2, 3] should be 13 because you swap 5 with -4
+*/
+
+var maxSliceSwap = arr => {
+
+  
+
+}
+
+export default maxSliceSwap;
