c69ef2e62777a49f0bbf51cad870a15fb3541143
Chris Chiang <cchiang1992@gmail.com>
2017-02-08 18:37:26 -0800

    Create test file for maxSliceSwap

--- src/maxSliceSwap.js
+++ src/maxSliceSwap.js
@@ -3,6 +3,7 @@
 /*
 ** Example
 ** [-3, 5, -2, 3, 4, -2, 3] should be 15 because you swap 5 with second -2
+** [-3, 5, -2, 3, 4, -1, -2, 3] should be 14 because you swap 5 with second -2
 ** [-3, 5, -2, 3, -4, 4, -2, 3] should be 13 because you swap 5 with -4
 ** [1, 1, 5, 2, -2, -2] should be 9. No swaps are required
 */
---
+++ tests/maxSliceSwapTest.js
@@ -0,0 +1,3 @@
+import { expect } from 'chai';
+import maxSliceSwap from '../src/maxSliceSwap';
+
