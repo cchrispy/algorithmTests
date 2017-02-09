f4c7c241645b231d08957738b01a0f0fee3398ae
Chris Chiang <cchiang1992@gmail.com>
2017-02-08 18:38:15 -0800

    Describe statement maxSliceSwapTest.js

--- tests/maxSliceSwapTest.js
+++ tests/maxSliceSwapTest.js
@@ -1,3 +1,10 @@
 import { expect } from 'chai';
 import maxSliceSwap from '../src/maxSliceSwap';
 
+describe ('Max Slice Swap', () => {
+
+  it('Should be a function', () => {
+    expect(maxSliceSwap).to.be.a('function');
+  })
+
+})
