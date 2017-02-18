d8dc348bf3d36236c69a35aa50d3f09fc3c8a950
Chris Chiang <cchiang1992@gmail.com>
2017-02-18 15:41:46 -0800

    Add test file for prefixMaxProduct

---
+++ tests/prefixMaxProductTest.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import prefixMaxProduct from '../src/prefixMaxProduct';
+
+describe('Prefix Max Product', () => {
+
+  it('Should be a function', () => {
+    expect(prefixMaxProduct).to.be.a('function');
+  })
+
+})
