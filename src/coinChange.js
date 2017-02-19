028b89f858db0f58c434f71612026b7d65deb11e
Chris Chiang <cchiang1992@gmail.com>
2017-02-18 16:19:43 -0800

    Add coinChange function

---
+++ src/coinChange.js
@@ -0,0 +1,21 @@
+/*
+Given a list of coin values, how many ways can you make change for a given amount N? 
+You may assume you have an infinite amount of coins of each value. 
+For example given a target amount, N = 4, and list of coin values, coins = [1, 2, 3], 
+there are four ways to make change for the amount 4: {1, 1, 1, 1}, {1, 1, 2}, {2, 2}, {1, 3}. 
+Try to optimize the solution. Hint: memoization.  
+
+Example:
+
+coinChange(4, [1, 2, 3]); // 4
+
+coinChange(10, [2, 3, 5, 6]); // 5
+// { 2, 2, 2, 2, 2}, { 2, 2, 3, 3 }, { 2, 2, 6 }, { 2, 3, 5 }, { 5, 5 }
+
+*/
+
+var coinChange = (n, coins) => {
+
+}
+
+export default coinChange;
