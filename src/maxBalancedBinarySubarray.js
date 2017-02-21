35046014f3f7d8fdcddc4da4427ee10c577133dc
Chris Chiang <cchiang1992@gmail.com>
2017-02-20 17:59:08 -0800

    Add maxBalancedBinarySubarray algorithm

---
+++ src/maxBalancedBinarySubarray.js
@@ -0,0 +1,23 @@
+/*
+** Given an array consisting of 0s and 1s, return the length of the longest 
+** subarray that contain an equal number of 0s and 1s.
+** EXAMPLE
+** Input: [1, 1, 1, 0, 1, 0, 0, 1]
+** Output: 6
+**
+** Input: [0, 1, 1, 1, 1, 0, 0, 1]
+** Output: 4
+**
+** Input: [0, 1, 1, 1, 1, 0]
+** Output: 2
+*/
+
+var maxBalancedBinarySubarray = arr => {
+
+
+
+}
+
+var sum = arr => arr.reduce((tot, cur) => tot + cur);
+
+export default maxBalancedBinarySubarray;
