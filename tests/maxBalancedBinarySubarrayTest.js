8d18723251193fed77c31c429819116944df3398
Chris Chiang <cchiang1992@gmail.com>
2017-02-20 18:05:40 -0800

    3 Test cases added

--- tests/maxBalancedBinarySubarrayTest.js
+++ tests/maxBalancedBinarySubarrayTest.js
@@ -7,4 +7,20 @@ describe('Max Balanced Binary Subarray', () => {
     expect(maxBalancedBinarySubarray).to.be.a('function');
   })
 
+  describe('Test cases', () => {
+
+    it('Case 1: [1, 1, 1, 0, 1, 0, 0, 1]', () => {
+      expect(maxBalancedBinarySubarray([1, 1, 1, 0, 1, 0, 0, 1])).to.equal(6);
+    })
+
+    it('Case 2: [0, 1, 1, 1, 1, 0, 0, 1]', () => {
+      expect(maxBalancedBinarySubarray([0, 1, 1, 1, 1, 0, 0, 1])).to.equal(4);
+    })
+
+    it('Case 3: [0, 1, 1, 1, 0]', () => {
+      expect(maxBalancedBinarySubarray([0, 1, 1, 1, 0])).to.equal(2);
+    })
+
+  })
+
 })
