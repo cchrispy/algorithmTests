365b7f81124b538d8c64b1db419a0b40e3698504
Chris Chiang <cchiang1992@gmail.com>
2017-02-20 18:04:11 -0800

    Add test file for maxBalancedBinarySubarray

---
+++ tests/maxBalancedBinarySubarrayTest.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import maxBalancedBinarySubarray from '../src/maxBalancedBinarySubarray';
+
+describe('Max Balanced Binary Subarray', () => {
+
+  it('Should be a function', () => {
+    expect(maxBalancedBinarySubarray).to.be.a('function');
+  })
+
+})
