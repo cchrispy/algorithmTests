9821c2313b8dcf6a4ee3d176ee476d37bed4c3e3
Chris Chiang <cchiang1992@gmail.com>
2017-02-21 18:23:59 -0800

    Implement magicIndex solution naive

--- src/magicIndex.js
+++ src/magicIndex.js
@@ -7,12 +7,22 @@
 ** EXAMPLE
 ** magicIndex([3, 4, 5, 6]) returns false
 ** magicIndex([-2, 0, 1, 2, 4, 5]) returns 4 or 5
+** magicIndex([3, 4, 4, 4, 4, 6]) returns 4
 */
 
 var magicIndex = arr => {
   
-  
-  
+  /* Solution with O(n) time complexity and NO recursion */
+  /*******************************************************/
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === i) {
+      return i;
+    }
+  }
+  return false;
+  /*******************************************************/
+  /*******************************************************/
+
 }
 
 export default magicIndex;
