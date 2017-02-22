dcbcf95ea1021c7ec6f76242211f04ce79149122
Chris Chiang <cchiang1992@gmail.com>
2017-02-21 18:12:19 -0800

    Write magicIndex problem, commented

---
+++ src/magicIndex.js
@@ -0,0 +1,10 @@
+/*
+** A magic index in an array A[0...n-1] is defined to be an index such that A[i] = i.
+** Given a SORTED array of DISTINCT integers, write a method to find a magic index,
+** if one exists, in array A.
+** EXTRA: What if the values are not distinct?
+*/
+
+var magicIndex = arr => {
+  
+}
