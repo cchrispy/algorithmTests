5ec11fed4b5660210e7bd56ed61d348710e90627
Chris Chiang <cchiang1992@gmail.com>
2017-02-21 18:18:31 -0800

    Examples listed

--- src/magicIndex.js
+++ src/magicIndex.js
@@ -1,8 +1,12 @@
 /*
 ** A magic index in an array A[0...n-1] is defined to be an index such that A[i] = i.
 ** Given a SORTED array of DISTINCT integers, write a method to find a magic index,
-** if one exists, in array A.
+** if one exists, in array A. If it does not exist, return false.
 ** EXTRA: What if the values are not distinct?
+**
+** EXAMPLE
+** magicIndex([3, 4, 5, 6]) returns false
+** magicIndex([-2, 0, 1, 2, 4, 5]) returns 4 or 5
 */
 
 var magicIndex = arr => {
