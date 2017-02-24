862ad2c81ab7cea17e9893515a54a9de8c75fd89
Chris Chiang <cchiang1992@gmail.com>
2017-02-23 16:54:15 -0800

    castleOnAGrid problem detailed

--- src/castleOnAGrid.js
+++ src/castleOnAGrid.js
@@ -1,3 +1,22 @@
 /*
-** 
+** You are given a N x N grid or matrix, with a start (castle) and end coordinate. Write a function that
+** will calculate the minimum number of steps to move the castle from the start to the end.
+** The castle can move to any coordinate in one move if there is a clear straight path. Some cells 
+** on the grid are forbidden and the castle cannot cross/touch them.
+** Forbidden cells are denoted as 0. Allowed cells are denoted as 1.
+**
+** EXAMPLE
+** var grid = [
+**  [1, 0, 1],
+**  [1, 0, 1],
+**  [1, 1, 1],
+** ];
+** Start coordinate (0,0). End coordinate (2, 0).
+** Output: 3. (The path goes from (0,0) -> (2, 0) -> (2, 2) -> (2, 0))
 */
+
+var castleOnAGrid = (grid, start, end) => {
+
+}
+
+export default castleOnAGrid;
