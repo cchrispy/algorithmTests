7e425b8a3f24949febd5aa32c44cf6499f9cabfe
Chris Chiang <cchiang1992@gmail.com>
2017-02-24 18:12:43 -0800

    Add extra credit constraints

--- src/permutationsString.js
+++ src/permutationsString.js
@@ -1,7 +1,7 @@
 /*
 ** Given a string of unique characters, write a method to compute all
 ** unique permutations of the string.
-** EXTRA: The input string may have duplicate characters. Return all unique permutations
+** EXTRA: The input string may have duplicate characters. Return all unique permutations.
 **
 ** EXAMPLE
 ** Input: 'abc'
