8d5bec609f583e0fff9cf9515695663ac209f3c1
Chris Chiang <cchiang1992@gmail.com>
2017-02-24 17:58:22 -0800

    PermutationString algo

---
+++ src/permutationsString.js
@@ -0,0 +1,17 @@
+/*
+** Given a string of unique characters, write a method to compute all
+** unique permutations of the string.
+** EXTRA: The input string may have duplicate characters. Return all unique permutations
+**
+** EXAMPLE
+** Input: 'abc'
+** Output: 'abc', 'acb', 'bac', 'bca', 'cab', 'cba'
+*/
+
+const permutationsString = (str, res = []) => {
+
+  
+
+}
+
+export default permutationsString;
