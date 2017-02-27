8934235a46a6697c62343ae5c4f3d44620c03c21
Chris Chiang <cchiang1992@gmail.com>
2017-02-26 17:52:48 -0800

    Commented permutationsString

--- src/permutationsString.js
+++ src/permutationsString.js
@@ -10,22 +10,29 @@
 
 const permutationsString = (str, res = []) => {
   
-  if (!str.length) {
+  if (!str.length) { // if the string is empty, return the results
     return res;
   }
-  if (!res.length) {
+  if (!res.length) { // if the results is empty, add the first letter in
     res.push(str[0]);
   } else {
-    let letter = str[0];
-    let copy = res.slice();
-    res = [];
+
+    let letter = str[0]; // grab the next letter
+    let copy = res.slice(); // make a copy of the results
+    res = []; // reset the results array
+
+    // iterate through each existing permutation
     copy.forEach(perm => {
+      // for each permutation, create another string by adding the new letter
+      // to every index of the existing string, thus generating a brand new
+      // permutation with the new letter added in
       for (let i = 0; i <= perm.length; i++) {
         var newString = perm.slice(0, i) + letter + perm.slice(i);
         res.push(newString);
       }
     })
   }
+  // recurse until all letters of the original string is used up, then return the result
   return permutationsString(str.slice(1), res);
 }
 
