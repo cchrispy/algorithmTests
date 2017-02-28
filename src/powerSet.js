b81503bcab59695533dac3f3298ddbda83bb49c8
Chris Chiang <cchiang1992@gmail.com>
2017-02-27 18:39:54 -0800

    Add example to problem

--- src/powerSet.js
+++ src/powerSet.js
@@ -1,5 +1,9 @@
 /*
 ** Write a method to return all subsets of a set
+**
+** EXAMPLE
+** Input: [1, 2, 3]
+** Output: [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ]
 */
 
 var powerSet = set => {
