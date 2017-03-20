a74d77884b789f49af575be0ba3de0ca6342e542
Chris Chiang <cchiang1992@gmail.com>
2017-03-19 17:52:05 -0700

    Test cases for saywhatyousee

--- tests/sayWhatYouSeeTest.js
+++ tests/sayWhatYouSeeTest.js
@@ -5,6 +5,25 @@ describe('Say What You See', () => {
 
   it('Should be a function', () => {
     expect(sayWhatYouSee).to.be.a('function');
+  });
+
+  describe('Test cases', () => {
+
+    it('Case 1: ["12", "21"]', () => {
+      var solution = sayWhatYouSee(["12", "21"]);
+      expect(solution).to.have.members(['1112', '1211']);
+    })
+
+    it('Case 2: ["1112", "1133"]', () => {
+      var solution = sayWhatYouSee(["1112", "1133"]);
+      expect(solution).to.have.members(['3112', '2123']);
+    })
+
+    it('Case 3: ["11114", "123"]', () => {
+      var solution = sayWhatYouSee(["11114", "123"]);
+      expect(solution).to.have.members(['4114', '111213']);
+    })
+
   })
 
 })
