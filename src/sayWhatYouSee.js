8d432a3515c1fefaaf70eb92a31c9c71d711f69e
Chris Chiang <cchiang1992@gmail.com>
2017-03-19 17:46:32 -0700

    Add saywhatyousee

---
+++ src/sayWhatYouSee.js
@@ -0,0 +1,13 @@
+// Say what you see
+// Transform a string of digits based on its spoken description
+// EXAMPLE:
+// 21 -> 'one two, one one' -> 1211
+// 12 -> 'one one, one two' -> 1112
+// Given an array of strings (digit based), return an array of strings (description based)
+// sayWhatYouSee(['12', '21', '1113']) // -> ['1112', '1211', '3113']
+
+var sayWhatYouSee = arr => {
+
+}
+
+export default sayWhatYouSee;
