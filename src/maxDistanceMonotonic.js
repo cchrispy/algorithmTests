872840b7cca8c0b6e3e624de542bc398718f06a0
Chris Chiang <cchiang1992@gmail.com>
2017-04-03 17:36:17 -0700

    Add maxdistancemonotonic algo

---
+++ src/maxDistanceMonotonic.js
@@ -0,0 +1,22 @@
+/*
+** A monotonic pair in an array A of length N is a pair of integers (P, Q) 
+** such that 0 <= P <= Q <= N and A[P] <= A[Q].
+** Given an array of integers, find the monotonic pair whose indeces 
+** are furthest apart and return that distance. 
+** Constraints: linear time complexity and linear space complexity.
+
+** EXAMPLE
+** var arr = [5, 3, 6, 3, 4, 2];
+** 
+** ** There are 11 monotonic pairs: 
+** ** (0,0), (0, 2), (1, 1), (1, 2), (1, 3), (1, 4), (2, 2), (3, 3), (3, 4), (4, 4), (5, 5)
+** ** The biggest distance is 3, in the pair (1, 4)
+** 
+** maxDistanceMonotonic(arr); // 3
+*/
+
+const maxDistanceMonotonic = arr => {
+
+}
+
+export default maxDistanceMonotonic;
