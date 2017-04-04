d564bd182d3fdce89a1d97e1b5c367fc858cc49a
Chris Chiang <cchiang1992@gmail.com>
2017-04-04 16:44:07 -0700

    Add leader algo

---
+++ src/leader.js
@@ -0,0 +1,15 @@
+/*
+** Given an array of integers, return the index of any element that appears
+** in more than half of the elements in the array.
+** Constraints: Linear time complexity and constant space
+**
+** EXAMPLE
+** var arr = [3, 4, 3, 2, 3, -1, 3, 3];
+** leader(arr); // returns 0, 2, 4, 6, or 7 because 3 appears 5 times
+*/
+
+var leader = arr => {
+  
+}
+
+export default leader;
