4f438d96e91c5e75bddb92c44c0372bd920f54e5
Chris Chiang <cchiang1992@gmail.com>
2017-04-24 17:27:14 -0700

    mergeOverlappingIntervals solution

--- src/mergeOverlappingIntervals.js
+++ src/mergeOverlappingIntervals.js
@@ -7,19 +7,31 @@
 
 var mergeOverlappingIntervals = intervals => {
 
-  // assuming the intervals are sorted
+  // assuming the intervals are sorted, this operates at O(n) time complexity
 
   return intervals.reduce((res, interval) => {
     if (!res.length) {
       return interval;
     }
 
-    
-    
+    var last = getLast(res);
+    if (interval[0] <= last[1]) {
+      return replaceLast(res, [last[0], interval[1]]);
+    } else {
+      return res.concat([interval]);
+    }
   }, [])
 }
 
 // get the last element in an array
 var getLast = arr => arr[arr.length - 1];
 
-export default mergeOverlappingIntervals;
+// replace the last element in an array with something else
+var replaceLast = (arr, item) => {
+  arr[arr.length - 1] = item;
+}
+
+let intervals = [[1, 5], [2, 3], [4, 6], [7, 8], [8, 10], [12, 15]];
+console.log(mergeOverlappingIntervals(intervals));
+
+// export default mergeOverlappingIntervals;
