80a9cdcfe5a8bf2a2b96cff33eccb02bdee7de6d
Chris Chiang <cchiang1992@gmail.com>
2017-05-04 14:38:13 -0700

    Init maxNonAdjacentSubSeqSup

---
+++ src/maxNonAdjacentSubSeqSum.js
@@ -0,0 +1,8 @@
+// Given an array of integers, find the maximum sum of a 
+// subsequence where the subsequence contains no adjacent elements.
+
+var maxNonAdjacentSubSeqSum = arr => {
+  
+}
+
+// export default maxNonAdjacentSubSeqSum;
